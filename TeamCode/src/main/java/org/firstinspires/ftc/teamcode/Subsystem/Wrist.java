85790e6eff83d4b521c65182c76a5a0a05df0321
Mauric3M0ss <aj.780875@gmail.com>
2025-09-06 14:14:59 -0600

    Add tony code :)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystem/Wrist.java
@@ -0,0 +1,54 @@
+package org.firstinspires.ftc.teamcode.Subsystem;
+
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.ServoImplEx;
+
+import org.firstinspires.ftc.teamcode.Util.RobotStates;
+
+public class Wrist {
+    private ServoImplEx wristServo;
+
+    private RobotStates.Wrist currentWristState = RobotStates.Wrist.FLOOR;
+    private double wristPos;
+
+    public void init(HardwareMap hardwareMap) {
+        this.wristServo = hardwareMap.get(ServoImplEx.class,"servoWristPosSet");
+        this.wristServo.setDirection(Servo.Direction.FORWARD);
+    }
+
+    public void setState(RobotStates.Wrist desiredWristState) {
+        this.currentWristState = desiredWristState;
+    }
+
+    public RobotStates.Wrist getCurrentState() {
+        return currentWristState;
+    }
+
+    public double getWristPosition(RobotStates.Wrist desiredWristState) {
+        switch(desiredWristState) {
+            case FLOOR:
+                this.wristPos = 1;
+                break;
+
+            case SCORE:
+                this.wristPos = 0;
+                break;
+        }
+        return wristPos;
+    }
+
+    public void goToState(Gamepad gamepad) {
+
+        if (gamepad.right_bumper) {
+            this.setState(RobotStates.Wrist.SCORE);
+        } else if (gamepad.left_bumper) {
+            this.setState(RobotStates.Wrist.FLOOR);
+        }
+
+        RobotStates.Wrist desiredWristState = this.getCurrentState();
+        double desiredWristPos = this.getWristPosition(desiredWristState);
+        this.wristServo.setPosition(desiredWristPos);
+    }
+}

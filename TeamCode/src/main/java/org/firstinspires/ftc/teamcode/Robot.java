39004ef87b86cddf6c5fc372d0949cde99088b2f
Calstar9000 <Calstar9000+github@gmail.com>
2025-09-29 16:06:41 -0600

    rollback

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -10,31 +10,28 @@ import org.firstinspires.ftc.teamcode.Util.Toggle;
 
 @TeleOp
 public class Robot extends OpMode {
-//    private final Drivetrain drivetrain = new Drivetrain();
-//    private final Toggle drivetrainToggle = new Toggle();
-//    private final Toggle raiseToggle = new Toggle();
-    private aprilTags aprilTags;
-
+    private final Drivetrain drivetrain = new Drivetrain();
+    private final Toggle drivetrainToggle = new Toggle();
+    private final Toggle raiseToggle = new Toggle();
+    private final aprilTags aprilTags = new aprilTags();
+    
     public void init() {
-//        this.drivetrain.init(hardwareMap);
-        this.aprilTags = new aprilTags(hardwareMap);
+        this.drivetrain.init(hardwareMap);
+
+        this.aprilTags.init();
     }
 
     @Override
     public void loop() {
 
-//        this.drivetrain.mechanumDrive(
-//                gamepad1.left_stick_x,
-//                gamepad1.left_stick_y,
-//                gamepad1.right_stick_x,
-//                drivetrainToggle.toggleButton(gamepad1.a)
-//        );
+        this.drivetrain.mechanumDrive(
+                gamepad1.left_stick_x,
+                gamepad1.left_stick_y,
+                gamepad1.right_stick_x,
+                drivetrainToggle.toggleButton(gamepad1.a)
+        );
 
-        double[] tagData = aprilTags.getTagData();
-        telemetry.addData("Range", tagData[0]);
-        telemetry.addData("Bearing", tagData[1]);
-        telemetry.addData("Tag ID", tagData[2]);
-        telemetry.update();
+        this.aprilTags.loop();
 
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Util/IDs.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Util/IDs.java
@@ -6,8 +6,6 @@ public class IDs {
     public static final String BACK_LEFT_MOTOR_ID = "1";
     public static final String FRONT_RIGHT_MOTOR_ID = "2";
     public static final String BACK_RIGHT_MOTOR_ID = "3";
-
-    //TODO: Lets label where on the driver hub these are
     public static final String CONTROL_HUB = "Control hub";
     public static final String CONTROL_HUB_IMU = "Control hub IMU";
 

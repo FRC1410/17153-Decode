65367af80aeee639931e4fbbf0f9d62aee594f13
Calstar9000 <Calstar9000+github@gmail.com>
2025-09-29 18:10:26 -0600

    OMG IT WORKS IT WORKS THE APRIL TAGS WORK I CAN SOO THE TAGS CORRECTLY OMGOMGOMG i mean terrible fps but like OMGOMGIT WORKS ok also can we talk about the fact that the problem is it didnt like the resolution like WAT anyways im done

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.teamcode.Subsystem.Drivetrain;
 import org.firstinspires.ftc.teamcode.Subsystem.aprilTags;
@@ -13,12 +14,12 @@ public class Robot extends OpMode {
     private final Drivetrain drivetrain = new Drivetrain();
     private final Toggle drivetrainToggle = new Toggle();
     private final Toggle raiseToggle = new Toggle();
-    private final aprilTags aprilTags = new aprilTags();
+    private aprilTags aprilTags;
     
     public void init() {
 //        this.drivetrain.init(hardwareMap);
 
-        this.aprilTags.init();
+        this.aprilTags = new aprilTags(hardwareMap);
     }
 
     @Override
@@ -31,7 +32,14 @@ public class Robot extends OpMode {
 //                drivetrainToggle.toggleButton(gamepad1.a)
 //        );
 
-        this.aprilTags.loop();
+        double[] tagData = aprilTags.getTagData();
 
+        telemetry.addData("Range", tagData[0]);
+        telemetry.addData("Bearing", tagData[1]);
+        telemetry.addData("ID", tagData[2]);
+        telemetry.addData("Camera State", this.aprilTags.visionPortal.getCameraState());
+        telemetry.addData("Detection Count", this.aprilTags.aprilTag.getDetections().size());
+        telemetry.addData("FPS", this.aprilTags.visionPortal.getFps());
+        telemetry.update();
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystem/aprilTags.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystem/aprilTags.java
@@ -1,42 +1,68 @@
 package org.firstinspires.ftc.teamcode.Subsystem;
 
+import android.util.Size;
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagGameDatabase;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
 
 import java.util.List;
 
 public class aprilTags {
-    private VisionPortal visionPortal;
-    private AprilTagProcessor aprilTag;
+    public VisionPortal visionPortal;
+    public AprilTagProcessor aprilTag;
 
     public aprilTags(HardwareMap hardwareMapCool) {
-        this.aprilTag = AprilTagProcessor.easyCreateWithDefaults();
-        this.visionPortal = VisionPortal.easyCreateWithDefaults(
-                hardwareMapCool.get(WebcamName.class, "Webcam 1"),
-                this.aprilTag
-        );
-    }
+        try {
+            this.aprilTag = new AprilTagProcessor.Builder()
+                    .setTagFamily(AprilTagProcessor.TagFamily.TAG_36h11)
+                    .setDrawAxes(true)
+                    .setDrawCubeProjection(true)
+                    .setDrawTagOutline(true)
+                    .setDrawTagID(true)
+                    .setOutputUnits(DistanceUnit.INCH, AngleUnit.DEGREES)
+                    .build();
 
+            this.visionPortal = new VisionPortal.Builder()
+                    .setCamera(hardwareMapCool.get(WebcamName.class, "Webcam 1"))
+                    .addProcessor(this.aprilTag)
+                    .setCameraResolution(new Size(1280, 800))
+                    .enableLiveView(true)
+                    .setAutoStopLiveView(false)
+                    .build();
+        } catch (Exception e) {
+            throw new RuntimeException("Camera init failed: " + e.getMessage());
+        }
+    }
     public double[] getTagData() {
         List<AprilTagDetection> detections = this.aprilTag.getDetections();
+
+        // Return detection count as first value for debugging
+        if (detections.isEmpty()) {
+            return new double[]{-999, 0, 0}; // -999 = no detections at all
+        }
+
+        AprilTagDetection detection = detections.get(0);
+        double id = detection.id;
         double range = 0;
         double bearing = 0;
-        double id = 0;
 
-        for (AprilTagDetection detection : detections) {
-            id = detection.id;
-            if (detection.ftcPose != null) {
-                range = detection.ftcPose.range;
-                bearing = detection.ftcPose.bearing;
-                break;
-            }
+        if (detection.ftcPose != null) {
+            range = detection.ftcPose.range;
+            bearing = detection.ftcPose.bearing;
+        } else {
+            range = -888; // -888 = detection exists but no pose
+            bearing = -888;
         }
+
         return new double[]{range, bearing, id};
     }
 }

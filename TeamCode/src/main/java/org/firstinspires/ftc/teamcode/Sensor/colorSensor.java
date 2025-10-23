080d8f951cafa56a1be47bddfc7d1eed1f865895
Cade Ottele <cade.ottele@icloud.com>
2025-10-23 17:13:43 -0600

    I might revert my changes later, not sure if they are 100% good. It should theoretically give the base rgb values for colors pointed with the REV v2 color sensor.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Sensor/colorSensor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Sensor/colorSensor.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.Sensor;
 
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
+import com.qualcomm.robotcore.hardware.NormalizedRGBA;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
@@ -20,7 +21,15 @@ public class colorSensor {
     private int detectColour() {
         int colour = sensor.getNormalizedColors().toColor();
         return colour;
+
     }
+    public void updateRGB() {
+        NormalizedRGBA colors = sensor.getNormalizedColors();
+        colourRed = (int)(colors.red * 255);
+        colourGre = (int)(colors.green * 255);
+        colourBlu = (int)(colors.blue * 255);
+    }
+
     public void colourData(Telemetry telemetry) {
         telemetry.addData("Colour: ", this.detectColour());
         telemetry.update();

b3476138bc3e018ba895b345e6618671acc2591b
Mauric3M0ss <aj.780875@gmail.com>
2025-10-28 11:28:13 -0600

    Color sensor returns overcorrected values, need to normalize

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Sensor/colorSensor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Sensor/colorSensor.java
@@ -3,43 +3,74 @@ package org.firstinspires.ftc.teamcode.Sensor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
 import com.qualcomm.robotcore.hardware.NormalizedRGBA;
+import com.qualcomm.hardware.rev.RevColorSensorV3;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 public class colorSensor {
     NormalizedColorSensor sensor;
-    public static double colourRed;
-    public static double colourBlu;
-
-    public static double colourGre;
-
-
+    public static int colourRed;
+    public static int colourBlu;
+    public static int colourGre;
 
     public void init(HardwareMap hardwareMap, String COLOUR_SENSOR_ID) {
         sensor = hardwareMap.get(NormalizedColorSensor.class, COLOUR_SENSOR_ID);
+        // Try to enable LED if it's a Rev Color Sensor V3
+        try {
+            RevColorSensorV3 revSensor = hardwareMap.get(RevColorSensorV3.class, COLOUR_SENSOR_ID);
+            revSensor.enableLed(true);
+        } catch (Exception e) {
+            // Sensor doesn't support LED control, continue without it
+        }
     }
-    public int detectColour() {
-        int colour = sensor.getNormalizedColors().toColor();
-        return colour;
 
+    public int detectColour() {
+        updateRGB();
+        // Simple color detection based on which channel is strongest
+        if (colourRed > colourGre && colourRed > colourBlu) return 1; // RED
+        if (colourGre > colourRed && colourGre > colourBlu) return 2; // GREEN
+        if (colourBlu > colourRed && colourBlu > colourGre) return 3; // BLUE
+        return 0;
     }
+
     public void updateRGB() {
         NormalizedRGBA colors = sensor.getNormalizedColors();
-        colourRed = (int)(colors.red * 255);
-        colourGre = (int)(colors.green * 255);
-        colourBlu = (int)(colors.blue * 255);
+        float r = colors.red;
+        float g = colors.green;
+        float b = colors.blue;
+
+        // Find the maximum channel value
+        float max = Math.max(r, Math.max(g, b));
+
+        if (max < 0.01f) {
+
+            colourRed = (int)(r * 2550);
+            colourGre = (int)(g * 2550);
+            colourBlu = (int)(b * 2550);
+        } else {
+
+            float normalizedR = r / max;
+            float normalizedG = g / max;
+            float normalizedB = b / max;
+
+
+            colourRed = (int)(normalizedR * 255);
+            colourGre = (int)(normalizedG * 255);
+            colourBlu = (int)(normalizedB * 255);
+        }
+
+
+        colourRed = Math.min(colourRed, 255);
+        colourGre = Math.min(colourGre, 255);
+        colourBlu = Math.min(colourBlu, 255);
     }
 
     public void colourData(Telemetry telemetry) {
+        telemetry.addData("------",1);
         updateRGB();
+        NormalizedRGBA colors = sensor.getNormalizedColors();
+        telemetry.addData("Raw Values", "(%.3f, %.3f, %.3f)", colors.red, colors.green, colors.blue);
         telemetry.addData("RGB", "(%d, %d, %d)", colourRed, colourGre, colourBlu);
         telemetry.update();
-
     }
-
 }
-
-
-
-
-

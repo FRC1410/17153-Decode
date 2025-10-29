411928b8c1b4c3b3de3c43ad855b041fc02f5d36
EonCuber28 <ic28017420@gmail.com>
2025-10-29 17:17:51 -0600

    finished the documentation for dynamite file format. also added WIP indicators to different things in docs.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dynamite/dynConfig.java
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.teamcode.dynamite;
+
+public class dynConfig {
+    public String dynID;
+    // WIP
+    // Will be what other scripts will use to have quick access to .dyn configs
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dynamite/dynParser.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dynamite/dynParser.java
@@ -26,7 +26,3 @@ public class dynParser {
         }
     }
 }
-
-class DynData{
-
-}

a07dbaedfb20bb089b64087c8a40376077f035d3
Greek Question Mark <galaxyisfake@gmail.com>
2025-11-07 17:43:56 -0700

    Added a cycle method for testing purposes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystem/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystem/Shooter.java
@@ -1,17 +1,20 @@
 package org.firstinspires.ftc.teamcode.Subsystem;
 
 import static com.qualcomm.robotcore.hardware.DcMotorSimple.Direction.FORWARD;
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
 import static org.firstinspires.ftc.teamcode.Util.IDs.SHOOTER_MOTOR_ID;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.Util.RobotStates;
 
 public class Shooter {
 
     private DcMotorEx motorShooter;
+    public RobotStates.ShooterStates shooterStatus = RobotStates.ShooterStates.FORWARD;
 
 
     public void init(HardwareMap hardwareMap) {
@@ -25,6 +28,25 @@ public class Shooter {
         this.motorShooter.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         this.motorShooter.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+
+    public void cycle(Telemetry telemetry) {
+        switch (shooterStatus) {
+            case FORWARD:
+                this.shooterStatus = RobotStates.ShooterStates.BACKWARD;
+                telemetry.addData("Drive Mode:", this.shooterStatus);
+            case BACKWARD:
+                this.shooterStatus = RobotStates.ShooterStates.NEUTRAL;
+                telemetry.addData("Drive Mode:", this.shooterStatus);
+            case NEUTRAL:
+                this.shooterStatus = RobotStates.ShooterStates.HALF_POWER;
+                telemetry.addData("Drive Mode:", this.shooterStatus);
+            case HALF_POWER:
+                this.shooterStatus = RobotStates.ShooterStates.FORWARD;
+                telemetry.addData("Drive Mode:", this.shooterStatus);
+        }
+        run(this.shooterStatus);
+        telemetry.update();
     }
 
     public void run(RobotStates.ShooterStates shooterState){

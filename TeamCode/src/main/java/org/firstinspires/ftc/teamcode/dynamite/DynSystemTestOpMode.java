98eba99b70644629f2188803a0e651d84c43f66b
EonCuber <ic28017420@gmail.com>
2026-01-15 15:39:10 -0700

    started work on test auto for work with pedropathng.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dynamite/DynSystemTestOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dynamite/DynSystemTestOpMode.java
@@ -30,7 +30,7 @@ public class DynSystemTestOpMode extends DynAutoOpMode {
 
     @Override
     protected String getScriptName() {
-        return "DynFullSystemTest.dyn";
+        return "AutoTest.dyn";
     }
 
     @Override

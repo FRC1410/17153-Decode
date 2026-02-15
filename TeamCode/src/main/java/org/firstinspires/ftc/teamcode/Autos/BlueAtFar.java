ae4786e97a44108e0ad1d394687089ad6582ca6e
EonCuber <ic28017420@gmail.com>
2026-02-15 09:06:36 -0700

    made auto ops and did as much as i can to mitigate localisation drift.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autos/BlueAtFar.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autos/BlueAtFar.java
@@ -1,9 +1,8 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.Autos;
 
 import com.pedropathing.geometry.Pose;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
-import org.firstinspires.ftc.teamcode.Subsystem.Intake;
 import org.firstinspires.ftc.teamcode.Subsystem.IntakeAuto;
 import org.firstinspires.ftc.teamcode.Subsystem.Shooter;
 import org.firstinspires.ftc.teamcode.Util.RobotStates;
@@ -11,8 +10,8 @@ import org.firstinspires.ftc.teamcode.dynamite.DynAutoOpMode;
 import org.firstinspires.ftc.teamcode.dynamite.DynCommands.Etc.CustomCommand;
 import org.firstinspires.ftc.teamcode.dynamite.DynVar.DynVar;
 
-//@Autonomous(name="Dyn Auto")
-public class DynAuto extends DynAutoOpMode{
+@Autonomous(name="Blue: Far")
+public class BlueAtFar extends DynAutoOpMode {
     public Shooter shooter = new Shooter();
     public IntakeAuto intake = new IntakeAuto();
 
@@ -34,26 +33,7 @@ public class DynAuto extends DynAutoOpMode{
 
     @Override
     protected String getScriptName(){
-        return "AutoTest.dyn";
-        /*
-        if (Alience == "BLUE"){
-            if (Pos == "At_Goal"){
-                return  "B_AG.dyn";
-            } else if (Pos == "At_Far"){
-                return "B_AF.dyn";
-            } else {
-                return "B_AS.dyn";
-            }
-        } else {
-            if (Pos == "At_Goal"){
-                return "R_AG.dyn";
-            } else if (Pos == "At_Far"){
-                return "R_AF.dyn";
-            } else {
-                return "R_AS.dyn";
-            }
-        }
-         */
+        return "B_AF.dyn";
     }
 
     @Override
@@ -96,6 +76,7 @@ public class DynAuto extends DynAutoOpMode{
             public DynVar execute(String functionName, DynVar input) {
                 // turn the intake on
                 intake.run(1,0);
+                safeSleep(500); // time for spool
                 return null;
             }
         });
@@ -112,26 +93,6 @@ public class DynAuto extends DynAutoOpMode{
             public DynVar execute(String functionName, DynVar input) {
                 long wait = (long)((double)input.toJava());
                 safeSleep(wait);
-                return null;
-            }
-        });
-        dynAuto.registerCustomCommand("getIntakeRPM",new CustomCommand.CustomCommandHandler() {
-            @Override
-            public DynVar execute(String functionName, DynVar input) {
-                try {
-                    return new DynVar("Number","",intake.getRPM());
-                } catch (Exception e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        });
-        dynAuto.registerCustomCommand("stopMotors", new CustomCommand.CustomCommandHandler() {
-            @Override
-            public DynVar execute(String functionName, DynVar input) {
-                intake.run(0,0);
-                shooter.run(RobotStates.ShooterStates.NEUTRAL);
-                shooter.feed(0);
-                safeSleep(1/20);
                 return null;
             }
         });

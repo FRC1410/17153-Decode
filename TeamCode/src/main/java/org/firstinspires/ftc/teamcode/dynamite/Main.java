d58611075b71f1815cc763003a6e3defaf7f9e3c
EonCuber28 <isaacacpersonal@gmail.com>
2026-08-10 20:44:38 -0600

    Fixed up some issues and work on DYN integration.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dynamite/Main.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dynamite/Main.java
@@ -12,60 +12,22 @@ import java.util.Scanner;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    private static FileReader fr;
-    private static String ScriptContents;
     private static Tokenizer tk;
     public static void main(String[] args) {
-        // scanner (just cus)
-        Scanner scanner = new Scanner(System.in);
-        // we now testing the file reader
-
-        // startup the scanner
-        //print("Path to scrips (leave empty for default): ");
-        String response = "";//scanner.nextLine();
-        if (response != null) {
-            if (!response.isEmpty()) {
-                fr = new FileReader(response);
-            } else {
-                fr = new FileReader();
-            }
-        } else {
-            fr = new FileReader();
-        }
-
-        // select the file, and start parsing
-        //print("script name (leave empty for default): ");
-        response = "";//scanner.nextLine();
-        if (response != null){
-            if (!response.isEmpty()){
-                ScriptContents = fr.readFile(response);
-            } else {
-                ScriptContents = fr.readFile("Test_claude");
-            }
-        } else {
-            ScriptContents = fr.readFile("Test_claude");
-        }
+        // load the script
+        FileReader fr = new FileReader();
+        String ScriptContents = fr.readFile("Main.dyn");
+        // link up the exceptions
         CommandException.linkFile(ScriptContents);
-        //println("Read Script: ");
-        //println(ScriptContents);
-        // test tokenizer
+        // Tokenize
         tk = new Tokenizer();
         Token[] tokens = tk.processScript(ScriptContents);
-        for (Token tk : tokens){
-            println(tk.toString());
-        }
-        println("");
+        // Construct the commands
         CommandConstructor cc = new CommandConstructor(ScriptContents);
         cc.processTokens(tokens);
-        println(cc.toString());
+        // Run the commands
         CommandRunner cr = new CommandRunner(cc);
         cr.linkUpCommand();
         cr.run();
-    }
-    private static void println(Object toBePrinted){
-        //System.out.println(toBePrinted);
-    }
-    private static void print(Object toBePrinted){
-        System.out.print(toBePrinted);
     }
 }

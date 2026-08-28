156c454dde625f23917cffcd57eab83ef285fd73
EonCuber28 <isaacacpersonal@gmail.com>
2026-08-27 21:30:08 -0600

    Architected DYNKindaInterface for user use with other functional auto FTC libs.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dynamite/InterfaceStuffs/DYNKindaInterface.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode.dynamite.InterfaceStuffs;
+
+public class DYNKindaInterface {
+    // this receives the given script (the class), and the interface class,
+    // and handles all operations related to running DYN
+    // DYN telemetry will be added like before, but UPDATING the telemetry will be handled by the user.
+
+    // this class will be required to be constructed with the following things: telemetry, and the script path, and optionally if the script is on a USB.
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dynamite/InterfaceStuffs/InterfaceUtils.java
@@ -0,0 +1,5 @@
+package org.firstinspires.ftc.teamcode.dynamite.InterfaceStuffs;
+
+public class InterfaceUtils {
+    // provide the DYN util commands for kindaInterface use
+}
